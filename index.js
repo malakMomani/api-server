b50f40b8f524dee763d411bc6b60875fbf96c0ad
Malak Al-Momani <malak.momani.96@gmail.com>
2021-06-23 01:48:42 +0300

    edit db uri

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = require('./src/server.js');
 
 const PORT = process.env.PORT || 5050;
 const MONGODB_URI = process.env.MONGODB_URI;
+
 // start server
 server.mongoConnect(MONGODB_URI);
 server.start(PORT);
